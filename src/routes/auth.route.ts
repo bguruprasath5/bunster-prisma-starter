08197e47c7b8b44176544be9790b1dc3c97644e5
dev-guruprasath <96348958+dev-guruprasath@users.noreply.github.com>
2023-09-21 15:56:49 +0530

    updated bunster v0.3.1

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,4 +1,5 @@
 import {
+  BunsterContext,
   BunsterJwt,
   BunsterRouteGroup,
   HttpError,
@@ -9,27 +10,29 @@ import { loginSchema } from '../models/auth.model';
 
 const router = new BunsterRouteGroup({ basePath: '/auth' });
 
+async function loginHandler(ctx: BunsterContext): Promise<Response> {
+  let user = await userRepository.getUserByEmail(ctx.body.email);
+
+  if (user && (await Bun.password.verify(ctx.body.password, user.password))) {
+    const token = await BunsterJwt.sign(
+      { id: user.id, email: user.email },
+      '1 day'
+    );
+    user = await userRepository.updateUser(user.id, { token });
+    return ctx.sendJson({
+      message: 'Login successful',
+      data: { ...user, password: undefined },
+    });
+  }
+  throw new HttpError('Invalid credentials', HttpStatus.BAD_REQUEST);
+}
+
 router.post({
   path: '/login',
   input: {
     body: loginSchema,
   },
-  handler: async (ctx) => {
-    let user = await userRepository.getUserByEmail(ctx.body.email);
-
-    if (user && (await Bun.password.verify(ctx.body.password, user.password))) {
-      const token = await BunsterJwt.sign(
-        { id: user.id, email: user.email },
-        '1 day'
-      );
-      user = await userRepository.updateUser(user.id, { token });
-      return ctx.sendJson({
-        message: 'Login successful',
-        data: { ...user, password: undefined },
-      });
-    }
-    throw new HttpError('Invalid credentials', HttpStatus.BAD_REQUEST);
-  },
+  handler: loginHandler,
 });
 
 export default router;
